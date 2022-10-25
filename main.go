6d95d08fe28bba036da0d6cf3279fbabbb634178
“Nawi” <nawihusen@gmail.com>
2022-10-25 14:29:51 +0800

    selesai

--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"strings"
 )
 
 func main() {
-	// fmt.Println(60494 + 17284)
+	aku := "asvnfkjjvdiljmdklc"
+	catch := SortString(aku)
+	fmt.Println(catch)
 }
 
 func SortString(w string) string {
 	s := strings.Split(w, "")
+	fmt.Println(s)
 	sort.Strings(s)
+	fmt.Println(s)
 	return strings.Join(s, "")
 }
