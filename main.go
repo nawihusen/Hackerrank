aaca850324fcc9301163e82be20ff304c14946f5
“Nawi” <nawihusen@gmail.com>
2022-11-02 07:48:35 +0800

    selesai

--- main.go
+++ main.go
@@ -6,9 +6,38 @@ import (
 	"strings"
 )
 
+func Bitw(a, b string) string {
+	result := ""
+	for k, _ := range a {
+		if string(a[k]) == "1" || string(b[k]) == "1" {
+			result += "1"
+		} else if string(a[k]) == "0" && string(b[k]) == "0" {
+			result += "0"
+		}
+	}
+
+	return result
+}
+
+func Count(a string) int {
+	result := 0
+	for _, v := range a {
+		if string(v) == "1" {
+			result += 1
+		}
+	}
+	return result
+}
+
 func main() {
-	test := [][]int{{1, 1}, {2, 2}}
-	fmt.Println(len(test))
+	satu := "00101"
+	dua := "00011"
+	tiga := Bitw(satu, dua)
+	con := Count(tiga)
+	fmt.Println(satu)
+	fmt.Println(dua)
+	fmt.Println(tiga)
+	fmt.Println(con)
 }
 
 func SortString(w string) string {
